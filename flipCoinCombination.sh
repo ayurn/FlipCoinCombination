255e1f9f008d29d6e040ae99605820c4db3d2e2e
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-09 18:23:18 +0530

    Added : Coin Flip sim

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,20 @@
 echo "Welcome to Flip coin combination problem"
 echo -e
+
+	headCount=0
+	tailCount=0
+
+	read -p "enter number of times to flip coin = " num
+	echo -e
+	for i in `seq $num`
+	do
+		flip=$((1+RANDOM%2))
+		if [ $flip -eq 1 ]
+		then
+			((headCount++))
+			singlet[H]=$headCount
+		else
+			((tailCount++))
+			singlet[T]=$tailCount
+		fi
+	done
