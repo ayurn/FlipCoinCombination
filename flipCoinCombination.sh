198437fa10040bc541909d3d86b053bcecf34245
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-09 18:33:49 +0530

    Added : Singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,5 +1,10 @@
+#!/bin/bash -x
+
 echo "Welcome to Flip coin combination problem"
-echo -e
+
+singlet_program ()
+{
+	declare -A singlet
 
 	headCount=0
 	tailCount=0
@@ -18,3 +23,24 @@ echo -e
 			singlet[T]=$tailCount
 		fi
 	done
+
+	echo "print singlet dictionary"
+
+	headsPercent=0;
+	tailsPercent=0;
+	for key in "${!singlet[@]}"
+	do
+		echo "$key : ${singlet[$key]}"
+		if [ "$key" = "H" ]
+		then
+			a=$(awk 'BEGIN {print '${singlet[$key]}' / '$num' }')
+			headsPercent=$(awk 'BEGIN {print '$a' * '100' }')
+		else
+			b=$(awk 'BEGIN {print '${singlet[$key]}' / '$num' }')
+			tailsPercent=$(awk 'BEGIN {print '$b' * '100' }')
+		fi
+	done
+
+	echo "Percentage of H = $headsPercent"
+	echo "Percentage of T = $tailsPercent"
+}
